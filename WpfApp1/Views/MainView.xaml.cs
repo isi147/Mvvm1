0944b94bd1a69fe77bf0642c2cd219ceb01475d4
Seyidmammadli_i <Seyidmammadli_i@STHQ011A-01>
2023-04-01 19:41:54 +0400

    Add project files.

---
+++ WpfApp1/Views/MainView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using WpfApp1.Command;
+
+namespace WpfApp1.Views
+{
+
+    public partial class MainView : Window
+    {
+        public ICommand ShowCommand { get; set; }
+        public MainView()
+        {
+            InitializeComponent();
+            DataContext = this;
+            ShowCommand = new RelayCommand(ExecuteShowCommand,CanExecuteShowCommand);
+        }
+        void ExecuteShowCommand(object? parametr)
+        {
+            MessageBox.Show(txtBox.Text);
+        }
+        bool CanExecuteShowCommand(object? parametr)
+            => txtBox.Text.Length>=5;
+
+    }
+}
